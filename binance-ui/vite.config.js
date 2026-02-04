b1dcf2847b1c20ddb8185f9ee46a84ef12d709ca
Amolika28-Kale <amolikakale234@gmail.com>
2026-02-04 21:15:50 +0530

    Fix: Added vite config and missing plugin

--- binance-ui/vite.config.js
+++ binance-ui/vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    strictPort: true,
-  },
-  resolve: {
-    alias: {
-      // जर पाथमध्ये अडचण येत असेल तर हे मदत करेल
-      '@': '/src',
-    },
-  },
+  build: {
+    outDir: 'dist', // Netlify साठी हे महत्वाचे आहे
+  }
 })
